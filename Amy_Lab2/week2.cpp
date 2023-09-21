bdcd86b3a4b8f496b4afcbcd9168ba8b4d8f827d
amydigitalportal <amy.digitalportal@gmail.com>
2023-09-21 15:45:46 -0400

    Initial commit (clean)

--- Amy_Lab2/week2.cpp
+++ Amy_Lab2/week2.cpp
@@ -12,33 +12,8 @@
 
 #include<iostream>
 
-using namespace std;
-
-class MyFirstClass 
-{ 
-private:
-	int m_age;
-	char *m_pFirstName, *m_pLastName;
-
-protected:
-	void setFirstName(char* f) { m_pFirstName = f; }
-	void setLastName(char* l) { m_pLastName = l; }
-
-public:
-	MyFirstClass() : MyFirstClass(0, nullptr, nullptr) { }
-	MyFirstClass(int age, char* firstName, char* lastName)
-	{
-		m_age = age;
-		m_pFirstName = firstName;
-		m_pLastName = lastName;
-	}
-};
-
-class MySecondClass { };
-
 int main(int argc, char **argv)
 {
-	MyFirstClass mfc;
-	cout << "Hello World!" << endl;
+	std::cout << "Hello World!" << std::endl;
 	return 0;
 }
