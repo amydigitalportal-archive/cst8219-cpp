eb54579ce7e0f30066a315ca4055573a13aad5bb
amydigitalportal <amy.digitalportal@gmail.com>
2023-09-21 16:03:56 -0400

    Demo #1

--- Amy_Lab2/week2.cpp
+++ Amy_Lab2/week2.cpp
@@ -15,41 +15,42 @@
 using namespace std;
 
 namespace CST8219 {
-	class Vehicle;
+
+	class Vehicle
+	{
+	private:
+		int numWheels, numDoors;
+
+	public:
+		Vehicle(int w, int d)
+		{
+			numWheels = w;
+			numDoors = d;
+
+			cout << "In constructor with 2 parameters" << endl;
+		}
+
+		Vehicle(int w) : Vehicle(w, 4)
+		{
+			cout << "In constructor with 1 parameters, wheels = " << w << endl;
+		}
+
+		Vehicle() : Vehicle(4)
+		{
+			cout << "In constructor with 0 parameters" << endl;
+		}
+
+		~Vehicle()
+		{
+			cout << "In destructor" << endl;
+		}
+	};
 }
 
-class Vehicle
-{
-private:
-	int numWheels, numDoors;
-
-public:
-	Vehicle(int w, int d) 
-	{
-		numWheels = w;
-		numDoors = d;
-
-		cout << "In constructor with 2 parameters" << endl;
-	}
-
-	Vehicle(int w) : Vehicle(w, 4)
-	{
-		cout << "In constructor with 1 parameters, wheels = " << w << endl;
-	}
-
-	Vehicle() : Vehicle(4) 
-	{
-		cout << "In constructor with 0 parameters" << endl;
-	}
-
-	~Vehicle() 
-	{
-		cout << "In destructor" << endl;
-	}
-};
 
 int main(int argc, char **argv)
 {
-	cout << "Hello World!" << endl;
+	CST8219::Vehicle myVehicle;
+	cout << "I made a Vehicle!" << endl;
 	return 0;
 }
