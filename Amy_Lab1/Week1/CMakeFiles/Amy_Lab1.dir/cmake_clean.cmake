f72236a3d76690aea3ecf4854084e0886d48e93d
Amy Novun <amy.digitalportal@gmail.com>
2023-09-21 11:34:44 -0400

    Add Lab 1 project

---
+++ Amy_Lab1/Week1/CMakeFiles/Amy_Lab1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "Amy_Lab1"
+  "Amy_Lab1.pdb"
+  "CMakeFiles/Amy_Lab1.dir/Week1.o"
+  "CMakeFiles/Amy_Lab1.dir/Week1.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Amy_Lab1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Amy_Lab1/Week1/Week1.cpp
@@ -0,0 +1,27 @@
+﻿// Week 1.cpp : Defines the entry point for the application.
+// CST8219 - C++ Programming (23F)
+// Student: Amy Novun - 040 757 422
+// Date: Sep 14, 2023
+
+#include "Week1.h"
+
+#include <iostream>
+
+//#define USING_NAMESPACES
+
+#ifdef USING_NAMESPACES
+using namespace std;
+#pragma message("Enabled using namespace 'std'.")
+#else
+#pragma message("Disabled using namespaces 'std'.")
+#endif
+
+int main()
+{
+#ifdef USING_NAMESPACES
+	cout << "Hello world! (using namespaces)" << endl;
+#else
+	std::cout << "Hello world! (not using namespaces)" << std::endl;
+#endif
+	return 0;
+}
