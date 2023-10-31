343fb3b0ea4b7434844235a5e9e672152077ccfd
Amy Novun <amy.digitalportal@gmail.com>
2023-10-31 14:23:00 -0400

    Create base cpp files for Vehicle subclasses

---
+++ Week6/src/GasolineVehicle.cpp
@@ -0,0 +1,16 @@
+/*File: GasolineVehicle.cpp
+*
+* Course:		CST8219 C++ Programming (23F)
+* Section:		302
+* Professor:	Eric Torunski
+*
+* Student:		Amy Novun - 040 757 422
+*
+* Assignment:	Lab Week 6
+* Due Date:		15 OCT 2023
+*/
+
+#include "../include/GasolineVehicle.h"
+
+using namespace std;
+using namespace CST8219;
