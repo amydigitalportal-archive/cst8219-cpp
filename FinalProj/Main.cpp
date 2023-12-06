721661571de2516854e16989ea965633a9c0828b
Amy Novun <amy.digitalportal@gmail.com>
2023-12-06 18:21:59 -0500

    GUI Built

--- FinalProj/Main.cpp
+++ FinalProj/Main.cpp
@@ -2,8 +2,9 @@
 #include <nana/gui/widgets/label.hpp>
 #include <nana/gui/widgets/button.hpp>
 #include <nana/gui/widgets/textbox.hpp>
+//#include "TestRunner.h"
+#include "Test.h"
 
-#include "TestRunner.h"
 
 #include <iostream>
 
@@ -11,47 +12,106 @@ using namespace std;
 
 #define STR_BTN_UNIFORM "Uniform"
 #define STR_BTN_NORMAL "Normal"
+#define STR_BTN_QUIT "Quit"
+
 #define LABEL_PROMPT "label_prompt"
-#define INPUT_TB "input_tb"
+#define TB_INPUT "tb_input"
 #define BTN_UNIFORM "btn_uniform"
 #define BTN_NORMAL "btn_normal"
+#define TB_OUTPUT "tb_output"
+#define BTN_QUIT "btn_quit"
 
-//#define LAUNCH_GUI
+#define LAUNCH_GUI
+
+void LogToConsole(nana::textbox* tb, std::string text)
+{
+    tb->append(text + "\n", false);
+}
+
+int ParseInt(std::string s)
+{
+    return stoi(s);
+}
+
+void RunUniformTests(int numSamples)
+{
+
+}
+
+void RunNormalTests(int numSamples)
+{
+
+}
+
+
+/*-- MAIN */
 
 int main()
 {
 #ifdef LAUNCH_GUI
     using namespace nana;
 
+    const nana::size WINDOW_SIZE{ 300, 400 };
+
     //Define a form.
     form fm;
+    fm.size(WINDOW_SIZE);
+    // Prevent window from resizing
+    // Ref: http://www.nanapro.org/en-us/forum/index.php?u=/topic/1069/how-to-prevent-resizing-a-form
+    API::track_window_size(fm, WINDOW_SIZE, false);
+    API::track_window_size(fm, WINDOW_SIZE, true);
 
     /*-- Label: Prompt n-samples input */
-    label label_prompt{ fm, "<bold blue size=16>Enter the number of samples to generate:</>" };
+    label label_prompt{ fm, "Enter the number of samples to generate:" };
     label_prompt.format(true);
+    label_prompt.text_align(nana::align::center, nana::align_v::center);
 
     /*-- TextBox: n-samples */
-    textbox tb{ fm, "Number of samples" };
+    textbox tb_input{ fm, "" };
+    tb_input.tip_string("Number of samples");
 
     /*-- Button: Uniform */
     button btn_uniform{ fm, STR_BTN_UNIFORM };
-    btn_uniform.events().click([&fm] {
-        cout << STR_BTN_UNIFORM << endl;
-        });
 
     /*-- Button: Normal */
     button btn_normal{ fm, STR_BTN_NORMAL };
-    btn_normal.events().click([] {
-        cout << STR_BTN_NORMAL << endl;
-        });
+    
+    /*-- TextBox: Output */
+    textbox* tb_output = new textbox{ fm, "" };
+    tb_output->editable(false);
+
+    /*-- Button: Quit */
+    button btn_quit{ fm, STR_BTN_QUIT };
 
     //Layout management
     //fm.div("vert <><<><weight=80% text><>><><weight=24<><button><>><>");
-    fm.div("vert <label_prompt><input_tb>< <btn_uniform> <btn_normal> >");
+    fm.div("vert margin=[0,10] <weight=10% margin=[10,0] label_prompt><weight=10% tb_input><weight=15% margin=[10,0] <btn_uniform> <btn_normal> > <tb_output> <weight=10% margin=[10,0] btn_quit>");
     fm[LABEL_PROMPT] << label_prompt;
-    fm[INPUT_TB] << tb;
+    fm[TB_INPUT] << tb_input;
     fm[BTN_UNIFORM] << btn_uniform;
     fm[BTN_NORMAL] << btn_normal;
+    fm[TB_OUTPUT] << *tb_output;
+    fm[BTN_QUIT] << btn_quit;
+
+    // -- Button Events
+
+    btn_uniform.events().click([&tb_input, &tb_output] {
+        cout << STR_BTN_UNIFORM << endl;
+
+        // If Input field is NOT empty...
+        if (tb_input.text().empty() == false)
+        {
+            LogToConsole(tb_output, tb_input.text());
+        }
+    });
+
+    btn_normal.events().click([&tb_input, &tb_output] {
+        cout << STR_BTN_NORMAL << endl;
+
+    });
+
+    btn_quit.events().click(API::exit); // Ref: https://stackoverflow.com/a/33582771
+
 
     fm.collocate();
 
@@ -61,15 +121,17 @@ int main()
     //Start to event loop process, it blocks until the form is closed.
     exec();
 #else
-    TestRunner* runner = new TestRunner();
-    runner->SetNumSamples(500);
+    //TestRunner* runner = new TestRunner();
+    //runner->SetNumSamples(500);
 
     //vector<int> myNums = runner->RunUniformTests();
-    vector<int> myNums = runner->RunNormalTests();
+    //vector<int> myNums = runner->RunNormalTests();
     
     //for (int n : myNums)
     //    cout << n << ' ';
     //cout << endl;
+
+    UniformTest u(1000);
 
     return 0;
 #endif
--- FinalProj/Test.h
+++ FinalProj/Test.h
@@ -1,22 +1,102 @@
 #ifndef TEST_H
 #define TEST_H
 
-
+#include <ctime>
+#include <iostream>
+#include <random>
 #include <vector>
+
+#define RANDOM_MIN -100
+#define RANDOM_MAX 100
 
 template <class T>
 class Test {
+private:
+	int numSamples;
+
 protected:
 	std::vector<T> numbers;
 
 public:
-	Test(int numSamples);
-	T GetMin();
-	T GetMax();
-	T GetMean();
-	T GetMedian();
+	Test(int numSamples) {
+		this->numSamples = numSamples;
+	}
+	//virtual ~Test() = 0;
 
-	std::vector<T> GetHistogram();
+	virtual T GetMin()
+	{
+		int smallest = 0;
+		for (int i : numbers)
+			if (i < smallest) smallest = i;
+		return smallest;
+	}
+
+	virtual T GetMax()
+	{
+		int largest = 0;
+		for (int i : numbers)
+			if (i > largest) largest = i;
+		return largest;
+	}
+
+	virtual T GetMean()
+	{
+		int sum = 0;
+		for (int i : numbers)
+			sum += i;
+		return static_cast<T>(sum / numbers.size());
+	}
+
+	virtual T GetMedian()
+	{
+		sort(numbers.begin(), numbers.end());
+
+		size_t n = numbers.size();
+		if (n % 2 == 0) // for even sample sizes...
+		{
+			T lowerMed = numbers[n / 2];
+			T upperMed = numbers[n / 2 + 1];
+			return (lowerMed + upperMed) / 2;
+		}
+		else // for odd sample sizes...
+		{
+			return numbers[n / 2];
+		}
+	}
+
+	virtual std::vector<T> GetHistogram()
+	{
+		std::vector<T> buckets;
+		const int numBuckets = 20;
+
+		T min = GetMin();
+		T max = GetMax();
+
+		return buckets;
+	}
+
+	static const int SAMPLE_MIN = 20;
+	static const int SAMPLE_MAX = 1000;
 };
+
+class UniformTest : public Test<int>
+{
+public:
+	UniformTest(int numSamples) : Test<int>(numSamples)
+	{
+		// Generate numbers based on the time
+		std::srand(static_cast<unsigned int>(time(NULL)));
+
+		for (int i = 0; i < numSamples; i++) {
+			int rNum = std::rand() % (RANDOM_MAX - RANDOM_MIN + 1) + RANDOM_MIN;
+			numbers.push_back(rNum);
+
+			std::cout << numbers[i] << std::endl;
+		}
+	}
+	~UniformTest() {}
+};
+
+
 
 #endif // !TEST_H
